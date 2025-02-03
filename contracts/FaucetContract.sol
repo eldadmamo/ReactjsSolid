dec9aede12575131679425656e21d0ba65f1f4d6
eldadstar <eldadfikre456@gmail.com>
2025-02-03 18:34:43 +0300

    private blockchain & truffle

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -4,5 +4,6 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Faucet {
     uint public funds = 1000;
     int public counter = -10;
-    uint32 public test = 4294967295
+    uint32 public test = 4294967295;
 }
+
