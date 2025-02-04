c68f917ff8e1c3622e3fc99a7921ec36e332db69
eldadstar <eldadfikre456@gmail.com>
2025-02-04 13:52:46 +0300

    Smart Contract

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -3,12 +3,24 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Faucet {
 
-    receive() external payable {
+    address[] public funders;
 
+    receive() external payable {}
+
+    function addFunds() external payable {
+        funders.push(msg.sender);
     }
 
-    function addFunds() external payable {}
+    function getAllFunders() public view returns(address[] memory){
+        return funders;
+    }
+
+    function getFunderAtIndex(uint index) external view returns(address){
+        address[] memory _funders = getAllFunders();
+        return _funders[index];
+    }
+
 }
 
 
-
+// 
